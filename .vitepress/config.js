97b73d24ad606e0f2c0d92ed40245f73ce6e2df1
TyCoding <tycoding@sina.com>
2021-08-08 17:34:53 +0800

    init

---
+++ .vitepress/config.js
@@ -0,0 +1,193 @@
+// @ts-check
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+ module.exports = {
+  base: '/',
+  title: 'Tumo-Cloud Docs',
+  lang: 'zh-CN',
+  description: 'Tumo-Cloud Docs',
+  head: createHead(),
+  themeConfig: {
+    repo: 'Tumo-Team/tumo-cloud',
+    docsRepo: 'Tumo-Team/tumo-cloud-docs',
+    logo: '/logo.png',
+    docsBranch: 'main',
+    editLinks: true,
+    editLinkText: '为此页提供修改建议',
+    nav: createNav(),
+    sidebar: createSidebar(),
+    pageHeadHtml: '<div><p align="center"><img src="http://cdn.tycoding.cn/MIK-WxRzP9.png" /></p><p align="center"><a                href="https://github.com/Tumo-Team"                target="_blank"              ><strong>Tumo Team —— Tumo-Boot</strong></a></p><ul><li>Tumo-Boot演示地址：<a                  href="http://boot.tycoding.cn"                  target="_blank"                  rel="noopener noreferrer"                >http://boot.tycoding.cn</a></li><li>Tumo-Boot在线文档：<a                  href="http://docs.boot.tycoding.cn"                  target="_blank"                  rel="noopener noreferrer"                >http://docs.boot.tycoding.cn</a></li><li>Tumo-Boot后端地址：<a                  href="https://github.com/Tumo-Team/Tumo-Boot"                  target="_blank"                  rel="noopener noreferrer"                >https://github.com/Tumo-Team/Tumo-Boot</a></li><li>Tumo-Boot前端地址：<a                  href="https://github.com/Tumo-Team/Tumo-Boot-UI"                  target="_blank"                  rel="noopener noreferrer"                >https://github.com/Tumo-Team/Tumo-Boot-UI</a></li><li>Tumo-Boot文档地址：<a                  href="https://github.com/Tumo-Team/Tumo-Boot-Docs"                  target="_blank"                  rel="noopener noreferrer"                >https://github.com/Tumo-Team/Tumo-Boot-Docs</a></li></ul>',
+  },
+};
+
+/**
+ * @type {()=>import('vitepress').HeadConfig[]}
+ */
+
+function createHead() {
+  return [
+    ['meta', { name: 'author', content: 'Vbenjs Team' }],
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content: 'tumo-cloud, springcloud, springboot, vite, ant-design-vue, vue',
+      },
+    ],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content:
+          'width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no',
+      },
+    ],
+    ['meta', { name: 'keywords', content: 'tumo cloud docs' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ];
+}
+
+/**
+ * @type {()=>import('./theme-default/config').DefaultTheme.NavItem[]}
+ */
+function createNav() {
+  return [
+    {
+      text: '项目介绍',
+      link: '/docs/intro/',
+      items: [
+        {
+          text: '项目介绍',
+          link: '/docs/intro/1-intro',
+        },
+        {
+          text: '文档介绍',
+          link: '/docs/intro/2-docs-introduce',
+        },
+        {
+          text: '代码生成',
+          link: '/docs/intro/3-generate',
+        },
+        {
+          text: '运行项目',
+          link: '/docs/intro/4-run',
+        },
+      ],
+    },
+    {
+      text: '微服务基础',
+      link: '/docs/base/',
+      items: [
+        {
+          text: '项目搭建',
+          link: '/docs/base/',
+        },
+        {
+          text: '模块化管理',
+          link: '/docs/base/',
+        },
+        {
+          text: '基础配置',
+          link: '/docs/base/',
+        },
+        {
+          text: '数据交互',
+          link: '/docs/base/',
+        },
+      ],
+    },
+    {
+      text: '微服务进阶',
+      link: '/docs/deep/',
+      items: [
+        {
+          text: '微服务通信',
+          link: '/docs/deep/',
+        },
+      ],
+    },
+  ];
+}
+
+function createSidebar() {
+  return {
+    '/docs/intro/': [
+      {
+        text: '项目介绍',
+        children: [
+          {
+            text: '项目介绍',
+            link: '/docs/intro/1-intro',
+          },
+          {
+            text: '文档介绍',
+            link: '/docs/intro/2-docs-introduce',
+          },
+          {
+            text: '代码生成',
+            link: '/docs/intro/3-generate',
+          },
+          {
+            text: '运行项目',
+            link: '/docs/intro/4-run',
+          },
+        ],
+      },
+    ],
+
+    '/docs/base': [
+      {
+        text: '前端设计',
+        children: [
+          {
+            text: '环境准备',
+            link: '/docs/app/1-design/1-1-environment',
+          },
+          {
+            text: '如何使用Vben项目',
+            link: '/docs/app/1-design/1-2-use-vben',
+          },
+          {
+            text: 'Tumo-Boot-UI项目搭建',
+            link: '/docs/app/1-design/1-3-init-tumo-boot-ui',
+          },
+        ],
+      },
+      
+    ],
+
+    '/docs/deep': [
+      {
+        text: '项目设计',
+        children: [
+          {
+            text: '环境准备',
+            link: '/docs/api/1-design/1-1-environment',
+          },
+          {
+            text: 'SpringBoot项目搭建',
+            link: '/docs/api/1-design/1-2-create-springboot',
+          },
+          {
+            text: 'Tumo-Boot项目搭建',
+            link: '/docs/api/1-design/1-3-init-tumo-boot',
+          },
+        ],
+      },
+      
+    ],
+  };
+}
+
+// /**
+//  * @type {(namespace:string,items:string[])=>string[]}
+//  */
+// function urlWrapper(namespace, items) {
+//   return items.map((item) => namespace + item);
+// }
+
+// function getGuildNav() {
+//   return urlWrapper('/guide', ['/']);
+// }
---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+    <img src="http://cdn.tycoding.cn/MIK-WxRzP9.png" />
+</p>
+<p align="center">
+    <a href="https://github.com/Tumo-Team" target="_blank">
+        <strong>Tumo Team —— Tumo-Cloud</strong>
+    </a>
+</p>
+
+
+- Tumo-Cloud演示地址：[http://cloud.tycoding.cn](http://cloud.tycoding.cn)
+- Tumo-Cloud在线文档：[http://docs.cloud.tycoding.cn](http://docs.cloud.tycoding.cn)
+- Tumo-Cloud后端地址：[https://github.com/Tumo-Team/tumo-cloud](https://github.com/Tumo-Team/tumo-cloud)
+- Tumo-Cloud前端地址：[https://github.com/Tumo-Team/tumo-cloud-ui](https://github.com/Tumo-Team/tumo-cloud-ui)
+- Tumo-Cloud文档地址：[https://github.com/Tumo-Team/tumo-cloud-docs](https://github.com/Tumo-Team/tumo-cloud-docs)
+
+
+# Tumo Cloud Docs
+
+```java
+public static void main(String[] args){
+    System.out.println("Hello Tumo Team!");
+}
+```
+
+**特点：**
+
+1. 代码简洁、规范。
+2. 提供最后端系统最基础的权限体系模块。
+3. 项目文档将涉及前后端、运维，带你一步步学习怎么优雅的开发。
+4. 基于SpringCloud最新版，单体架构，前后端分离。前端采用Ant-Design-Vue。
+5. 提供最最最基础、完善的项目开发文档，所有源码及文档都开源在GitHub上。
+
+## Docs
+
+## 本地运行
+
+```bash
+# 克隆本仓库
+$ git clone https://github.com/Tumo-Team/tumo-cloud-docs.git
+
+# 或者使用 npm
+$ yarn install
+
+# 启动开发服务器
+$ yarn dev
+```
